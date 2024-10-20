c07d20415f17e94900a950d69856dc17de0c82ba
DragonMage899 <justinbhana12@gmail.com>
2024-10-20 21:17:21 +0200

    Set up intial dockerfile and python print

---
+++ App/src/index.js
@@ -0,0 +1,10 @@
+// console.log('Hello!');
+// const { EventEmitter } = require('events');
+// const emitEvents = new EventEmitter();
+// emitEvents.on('rain', () => {
+//     console.log('');
+// })
+// emitEvents.emit('rain');
+// process.on('exit', function() {console.log('Good Bye')})
+
+const app = require('express')();
